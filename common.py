02bf2d12806caf65ef934ac6f87b3e19b1a0de73
Behnood Rasti <61419984+BehnoodRasti@users.noreply.github.com>
2022-08-27 07:53:34 +0200

    Add files via upload

---
+++ common.py
@@ -0,0 +1,222 @@
+import torch
+import torch.nn as nn
+import numpy as np
+#from downsampler import Downsampler
+class Downsampler(nn.Module):
+    '''
+        http://www.realitypixels.com/turk/computergraphics/ResamplingFilters.pdf
+    '''
+    def __init__(self, n_planes, factor, kernel_type, phase=0, kernel_width=None, support=None, sigma=None, preserve_size=False):
+        super(Downsampler, self).__init__()
+        
+        assert phase in [0, 0.5], 'phase should be 0 or 0.5'
+
+        if kernel_type == 'lanczos2':
+            support = 2
+            kernel_width = 4 * factor + 1
+            kernel_type_ = 'lanczos'
+
+        elif kernel_type == 'lanczos3':
+            support = 3
+            kernel_width = 6 * factor + 1
+            kernel_type_ = 'lanczos'
+
+        elif kernel_type == 'gauss12':
+            kernel_width = 7
+            sigma = 1/2
+            kernel_type_ = 'gauss'
+
+        elif kernel_type == 'gauss1sq2':
+            kernel_width = 9
+            sigma = 1./np.sqrt(2)
+            kernel_type_ = 'gauss'
+
+        elif kernel_type in ['lanczos', 'gauss', 'box']:
+            kernel_type_ = kernel_type
+
+        else:
+            assert False, 'wrong name kernel'
+            
+            
+        # note that `kernel width` will be different to actual size for phase = 1/2
+        self.kernel = get_kernel(factor, kernel_type_, phase, kernel_width, support=support, sigma=sigma)
+        
+        downsampler = nn.Conv2d(n_planes, n_planes, kernel_size=self.kernel.shape, stride=factor, padding=0)
+        downsampler.weight.data[:] = 0
+        downsampler.bias.data[:] = 0
+
+        kernel_torch = torch.from_numpy(self.kernel)
+        for i in range(n_planes):
+            downsampler.weight.data[i, i] = kernel_torch       
+
+        self.downsampler_ = downsampler
+
+        if preserve_size:
+
+            if  self.kernel.shape[0] % 2 == 1: 
+                pad = int((self.kernel.shape[0] - 1) / 2.)
+            else:
+                pad = int((self.kernel.shape[0] - factor) / 2.)
+                
+            self.padding = nn.ReplicationPad2d(pad)
+        
+        self.preserve_size = preserve_size
+        
+    def forward(self, input):
+        if self.preserve_size:
+            x = self.padding(input)
+        else:
+            x= input
+        self.x = x
+        return self.downsampler_(x)
+def add_module(self, module):
+    self.add_module(str(len(self) + 1), module)
+    
+torch.nn.Module.add = add_module
+
+class Concat(nn.Module):
+    def __init__(self, dim, *args):
+        super(Concat, self).__init__()
+        self.dim = dim
+
+        for idx, module in enumerate(args):
+            self.add_module(str(idx), module)
+
+    def forward(self, input):
+        inputs = []
+        for module in self._modules.values():
+            inputs.append(module(input))
+
+        inputs_shapes2 = [x.shape[2] for x in inputs]
+        inputs_shapes3 = [x.shape[3] for x in inputs]        
+
+        if np.all(np.array(inputs_shapes2) == min(inputs_shapes2)) and np.all(np.array(inputs_shapes3) == min(inputs_shapes3)):
+            inputs_ = inputs
+        else:
+            target_shape2 = min(inputs_shapes2)
+            target_shape3 = min(inputs_shapes3)
+
+            inputs_ = []
+            for inp in inputs: 
+                diff2 = (inp.size(2) - target_shape2) // 2 
+                diff3 = (inp.size(3) - target_shape3) // 2 
+                inputs_.append(inp[:, :, diff2: diff2 + target_shape2, diff3:diff3 + target_shape3])
+
+        return torch.cat(inputs_, dim=self.dim)
+
+    def __len__(self):
+        return len(self._modules)
+
+
+class GenNoise(nn.Module):
+    def __init__(self, dim2):
+        super(GenNoise, self).__init__()
+        self.dim2 = dim2
+
+    def forward(self, input):
+        a = list(input.size())
+        a[1] = self.dim2
+        # print (input.data.type())
+
+        b = torch.zeros(a).type_as(input.data)
+        b.normal_()
+
+        x = torch.autograd.Variable(b)
+
+        return x
+
+
+class Swish(nn.Module):
+    """
+        https://arxiv.org/abs/1710.05941
+        The hype was so huge that I could not help but try it
+    """
+    def __init__(self):
+        super(Swish, self).__init__()
+        self.s = nn.Sigmoid()
+
+    def forward(self, x):
+        return x * self.s(x)
+
+class ASC(nn.Module):
+    # def __init__(self):
+    #     self.s = nn.Hardtanh()
+    def __init__(self, a=3.5):
+        super().__init__()
+        self.a=a
+    def forward(self, input):
+        return torch.div(torch.exp(self.a*input),torch.sum(torch.exp(self.a*input), dim=1)) 
+       # return torch.div(torch.exp(x),torch.sum(torch.exp(x), dim=1)) 
+       # return torch.div(x,torch.sum(x, dim=1)+1e-20) 
+
+class Lin(nn.Module):
+    def __init__(self, n_feature, n_output):
+        super().__init__()
+        self.predict = torch.nn.Linear(n_feature, n_output)   # output layer
+
+    def forward(self, x):
+        x=x.view(x.size(0),-1)
+        x = self.predict(x)             # linear output
+        return x
+
+def act(act_fun = 'LeakyReLU'):
+    '''
+        Either string defining an activation function or module (e.g. nn.ReLU)
+    '''
+    if isinstance(act_fun, str):
+        if act_fun == 'LeakyReLU':
+            return nn.LeakyReLU(0.1, inplace=True)
+        elif act_fun == 'ReLU':
+            return nn.ReLU(inplace=True)
+        elif act_fun == 'Sigmoid':
+            return nn.Sigmoid()
+        elif act_fun == 'Hardtanh':
+            return nn.Hardtanh(min_val=-15,max_val=15)
+        elif act_fun == 'Tanh':
+            return nn.Tanh()
+        elif act_fun == 'softmax':
+            return nn.Softmax()
+        elif act_fun == 'Swish':
+            return Swish()
+        elif act_fun == 'ASC':
+            return ASC()
+        elif act_fun == 'ELU':
+            return nn.ELU()
+        elif act_fun == 'none':
+            return nn.Sequential()
+        else:
+            assert False
+    else:
+        return act_fun()
+
+
+def bn(num_features):
+    return nn.BatchNorm2d(num_features)
+
+
+def conv(in_f, out_f, kernel_size, stride=1, bias=True, pad='zero', downsample_mode='stride'):
+    downsampler = None
+    if stride != 1 and downsample_mode != 'stride':
+
+        if downsample_mode == 'avg':
+            downsampler = nn.AvgPool2d(stride, stride)
+        elif downsample_mode == 'max':
+            downsampler = nn.MaxPool2d(stride, stride)
+        elif downsample_mode  in ['lanczos2', 'lanczos3']:
+            downsampler = Downsampler(n_planes=out_f, factor=stride, kernel_type=downsample_mode, phase=0.5, preserve_size=True)
+        else:
+            assert False
+
+        stride = 1
+
+    padder = None
+    to_pad = int((kernel_size - 1) / 2)
+    if pad == 'reflection':
+        padder = nn.ReflectionPad2d(to_pad)
+        to_pad = 0
+  
+    convolver = nn.Conv2d(in_f, out_f, kernel_size, stride, padding=to_pad, bias=bias)
+
+
+    layers = filter(lambda x: x is not None, [padder, convolver, downsampler])
+    return nn.Sequential(*layers)
